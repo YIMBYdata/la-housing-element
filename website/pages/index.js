ceb8e831de7b975ad6f904d02d090c9b55aea077
Sidharth Kapur <sidharthkapur1@gmail.com>
2021-09-18 20:16:43 -0700

    Initial commit

---
+++ website/pages/index.js
@@ -0,0 +1,118 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+// import ReactMapboxGl, { Layer, Source, Popup, MapContext } from 'react-mapbox-gl'
+// import { LngLat } from 'mapbox-gl'
+// import 'mapbox-gl/dist/mapbox-gl.css'
+
+import { useMemo, useRef, useState, useCallback } from 'react'
+import DeckGL from '@deck.gl/react';
+import {StaticMap} from 'react-map-gl';
+import {GeoJsonLayer} from '@deck.gl/layers';
+import {_MapContext as MapContext} from 'react-map-gl';
+
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoic2lkLWthcCIsImEiOiJjamRpNzU2ZTMxNWE0MzJtZjAxbnphMW5mIn0.b6m4jgFhPOPOYOoaNGmogQ'
+
+// const Map = ReactMapboxGl({
+//   accessToken: MAPBOX_ACCESS_TOKEN
+// })
+
+const INITIAL_VIEW_STATE = {
+  longitude: -118.2467693,
+  latitude: 34.040713,
+  zoom: 10,
+  pitch: 0,
+  bearing: 0
+};
+
+const laBounds = [
+  [-118.66755417, 33.7064461],
+  [-118.1555711, 34.32457583],
+]
+
+function getTooltip({object}) {
+  if (object) {
+    const properties = object.properties
+    return {
+      html: `
+       <p>Current zoning: ${properties["zoning"]}</p>
+       <p>Proposed units: ${properties["units"]}</p>
+       <p>Proposed density: ${properties["density"]} units/acre</p>
+      `,
+      style: {
+        'background-color': 'white',
+        color: 'gray',
+        'line-height': '1em',
+        'font-size': '0.8em',
+      }
+    }
+  }
+}
+
+function getFillColor (object) {
+  const density = object.properties["density"]
+  const densityFraction = Math.min(density / 200, 1)
+  return [
+    (1 - densityFraction) * 255,
+    (1 - densityFraction) * 255,
+    240,
+  ]
+}
+
+function getText (object) {
+  return object.properties["units"]?.toString()
+}
+
+export default function Home() {
+  const [viewState, setViewState] = useState({zoom: 10})
+
+  const handleViewStateChange = useCallback(({viewState: nextViewState}) => {
+    setViewState(nextViewState)
+  }, [viewState])
+
+  const deckGlMap = (
+      <DeckGL
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+        getTooltip={getTooltip}
+        ContextProvider={MapContext.Provider}
+        onViewStateChange={handleViewStateChange}
+        >
+          <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+          <GeoJsonLayer
+              id='sites'
+              data='/out_smallest.geojson'
+              filled={true}
+              opacity={0.3}
+              getFillColor={getFillColor}
+              pickable={true}
+              />
+          <GeoJsonLayer
+            id='sites-text'
+            data='/points_smaller.geojson'
+            filled={false}
+            stroked={false}
+            getPosition={d => d.coordinates}
+            pointType='text'
+            getText={getText}
+            visible={viewState.zoom >= 16}
+            textSizeUnits='meters'
+            textSizeScale={0.25}
+            textFontFamily='sans-serif'
+          />
+      </DeckGL>
+  )
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Los Angeles Draft Housing Element - New Sites</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+      {deckGlMap}
+      </main>
+    </div>
+  )
+}
